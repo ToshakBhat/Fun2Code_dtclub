92b174e5279444067a0e40e18c05e682cf6d3b3c
Toshak <bhatsaab04@gmail.com>
2024-08-25 19:04:26 +0530

    added auto email and authorization - 2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "61ab6075e81338cf9f666d5c7503160d",
+"flutter_bootstrap.js": "3fe6a2adbc9e65ebfb7947ca15178e3f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "3629938d5de9052cb313b1fdaa68d42d",
 "/": "3629938d5de9052cb313b1fdaa68d42d",
-"main.dart.js": "34fa04cd123e34b31874da14abf72bbe",
+"main.dart.js": "b9d7a78dc02275bed3926bb1dedbec03",
 "manifest.json": "651199eab83f3e54684158ca3d4f383b",
 "version.json": "3b4e262779a327eae577211553aef5da"};
 // The application shell files that are downloaded before a service worker can
